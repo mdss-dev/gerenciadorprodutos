6b2dc5c4918dedad30109fcfb08ca58553e6a620
Michael Douglas <douglas.ti.dev@gmail.com>
2023-02-27 18:46:20 -0300

    Projeto iniciado

---
+++ src/main/java/com/md/gerenciadorprodutos/GerenciadorProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.md.gerenciadorprodutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorProdutosApplication.class, args);
+	}
+
+}
